5419761580f0eef11511e4570c28ec1d1c6d7eba
Pongpichat <pongpichat.sumetha2@gmail.com>
2021-01-19 17:50:29 +0700

    Commit get Event use to calendar

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,17 @@ import {
   Upload,
 } from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
+import VCalendar from "v-calendar";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 const app = createApp(App);
 app.use(DatePicker);
 app.use(Calendar);
 app.use(Button);
 app.use(Select);
 app.use(Tabs);
+app.use(VCalendar);
 app.use(Upload);
 app.use(Modal);
 app.use(Input);
